027b69b1289e30796b000d82c049f8beb86091e9
mathang <raclark36@gmail.com>
2025-12-31 12:54:55 +1000

    Set gemini-2.5-flash-lite as default model

--- netlify/functions/openrouter-chat.js
+++ netlify/functions/openrouter-chat.js
@@ -57,7 +57,8 @@ exports.handler = async (event, context) => {
     })}\n\n`;
 
   const models = [
-    "google/gemini-3-flash-preview",    
+    "google/gemini-2.5-flash-lite",
+    "google/gemini-3-flash-preview",
     "google/gemma-3-27b-it:free",
     "openai/gpt-oss-20b:free",
   ];
