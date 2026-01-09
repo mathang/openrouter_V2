6ced725881e143b2e6427f719d7f2f81bfff25a3
mathang <raclark36@gmail.com>
2026-01-09 16:35:54 +1000

    Update openrouter-chat.js

--- netlify/functions/openrouter-chat.js
+++ netlify/functions/openrouter-chat.js
@@ -57,7 +57,7 @@ export const handler = async (event, context) => {
     })}\n\n`;
 
   const models = [
-    "meta-llama/llama-4-maverick",
+    "x-ai/grok-4-fast",
     "google/gemini-2.5-flash-lite",
     "google/gemini-3-flash-preview",
     "google/gemma-3-27b-it:free",
