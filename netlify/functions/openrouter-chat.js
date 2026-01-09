3fd34e117034bc5f062bb65c11578afb98178f97
mathang <raclark36@gmail.com>
2026-01-09 15:53:54 +1000

    Comment out a model in openrouter-chat.js
    GPT-OSS-20B-FREE

--- netlify/functions/openrouter-chat.js
+++ netlify/functions/openrouter-chat.js
@@ -57,7 +57,8 @@ export const handler = async (event, context) => {
     })}\n\n`;
 
   const models = [
-    "google/gemini-2.5-flash-lite",
+    // "google/gemini-2.5-flash-lite",
+    "openai/gpt-oss-20b:free"
     "google/gemini-3-flash-preview",
     "google/gemma-3-27b-it:free",
     "openai/gpt-oss-20b:free",
